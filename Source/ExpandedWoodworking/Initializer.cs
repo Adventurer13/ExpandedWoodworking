ba91496bb9c7880ae26dfd437b3a06a099b090c6
KennethSamael <58892009+KennethSamael@users.noreply.github.com>
2020-10-23 22:02:51 +0200

    Add files via upload

---
+++ Source/ExpandedWoodworking/Initializer.cs
@@ -0,0 +1,15 @@
+﻿using RimWorld;
+using Verse;
+
+namespace ExpandedWoodworking
+{
+    [StaticConstructorOnStartup]
+    static class Initializer
+    {
+        static Initializer()
+        {
+            PlaceholderLogPatcher placeholderLogPatcher = new PlaceholderLogPatcher();
+            placeholderLogPatcher.Patcher();
+        }
+    }
+}
---
+++ Source/ExpandedWoodworking/PlaceholderLogPatcher.cs
@@ -0,0 +1,21 @@
+﻿using RimWorld;
+using Verse;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ExpandedWoodworking
+{
+    public class PlaceholderLogPatcher
+    {
+        public void Patcher()
+        {
+            IEnumerable<ThingDef> treesUsingVanillaLogs = DefDatabase<ThingDef>.AllDefsListForReading.Where(x => x.plant != null && x.plant.harvestedThingDef != null && x.plant.harvestedThingDef == ThingDef.Named("WoodLog"));
+            foreach (ThingDef tree in treesUsingVanillaLogs)
+            {
+                tree.plant.harvestedThingDef = ThingDef.Named("WoodLog_Generic");
+            }
+        }
+
+    }
+}
